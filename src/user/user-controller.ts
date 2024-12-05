cf909ff7ca7a830b697112136ec667791430bedc
Lovish Duggal <lovishduggal11@gmail.com>
2024-12-05 18:05:38 +0530

    feat: add updateUser functionality

--- src/user/user-controller.ts
+++ src/user/user-controller.ts
@@ -43,3 +43,66 @@ export async function createUser(
         return next(error);
     }
 }
+
+export async function updateUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const { name, email, phone } = req.body;
+    const userId = req.params.id; // Get user ID from request parameters
+
+    // Validate user ID
+    const idRegex = /^[0-9a-fA-F]{24}$/;
+    if (!idRegex.test(userId)) {
+        return next(createHttpError(400, 'Invalid user ID format.'));
+    }
+
+    // Validate name (if provided)
+    if (name) {
+        const nameRegex = /^[A-Za-z\s]{3,50}$/;
+        if (!nameRegex.test(name)) {
+            return next(
+                createHttpError(
+                    400,
+                    'Name must be between 3 and 50 characters.'
+                )
+            );
+        }
+    }
+
+    // Validate email (if provided)
+    if (email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return next(createHttpError(400, 'Invalid email format.'));
+        }
+    }
+
+    // Validate phone (if provided)
+    if (phone) {
+        const phoneRegex = /^\d{10}$/;
+        if (!phoneRegex.test(phone)) {
+            return next(
+                createHttpError(400, 'Phone number must be 10 digits.')
+            );
+        }
+    }
+
+    try {
+        const existingUser = await userModel.findByIdAndUpdate(
+            userId,
+            { name, email, phone },
+            { new: true, runValidators: true }
+        );
+        if (!existingUser) {
+            return next(createHttpError(404, 'User not found.'));
+        }
+
+        return res
+            .status(200)
+            .json({ data: existingUser, message: 'User updated successfully' });
+    } catch (error) {
+        return next(error);
+    }
+}
--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -1,8 +1,9 @@
 import express, { type RequestHandler } from 'express';
-import { createUser } from './user-controller';
+import { createUser, updateUser } from './user-controller';
 
 const userRouter = express.Router();
 
 userRouter.post('/', createUser as unknown as RequestHandler);
+userRouter.put('/:id', updateUser as unknown as RequestHandler);
 
 export default userRouter;
